20c7a64b7b52ba29a73bc6211399ca7dbdbac89e
braham_prakash <braham.prakash@pristyncare.com>
2021-11-28 18:00:17 +0530

    respnsive rules added

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
+  const handleHamClick = () => {
+    const header = document?.querySelector("header");
+    header.classList.toggle("hide-header");
+  };
+
   useEffect(() => {
+    const header = document?.querySelector("header");
+    const nav = document?.querySelector("header nav");
     window.onscroll = () => {
       const { scrollY } = window;
 
-      if (scrollY > 100)
-        document.querySelector("header nav").classList.add("show-bg");
-      else document.querySelector("header nav").classList.remove("show-bg");
+      if (scrollY > 100) nav.classList.add("show-bg");
+      else nav.classList.remove("show-bg");
     };
+
+    header.classList.add("hide-header");
   }, []);
 
   return (
@@ -21,6 +29,31 @@ const Header = () => {
         <meta name="description" content="This is Adomoto" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
+
+      <div className="ham-menu-container">
+        <button className="ham-icon" onClick={handleHamClick}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 6C2 5.44772 2.44772 5 3 5H21C21.5523 5 22 5.44772 22 6C22 6.55228 21.5523 7 21 7H3C2.44772 7 2 6.55228 2 6Z"
+              fill="currentColor"
+            />
+            <path
+              d="M2 12.0322C2 11.4799 2.44772 11.0322 3 11.0322H21C21.5523 11.0322 22 11.4799 22 12.0322C22 12.5845 21.5523 13.0322 21 13.0322H3C2.44772 13.0322 2 12.5845 2 12.0322Z"
+              fill="currentColor"
+            />
+            <path
+              d="M3 17.0645C2.44772 17.0645 2 17.5122 2 18.0645C2 18.6167 2.44772 19.0645 3 19.0645H21C21.5523 19.0645 22 18.6167 22 18.0645C22 17.5122 21.5523 17.0645 21 17.0645H3Z"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
+      </div>
 
       <header>
         <nav>
